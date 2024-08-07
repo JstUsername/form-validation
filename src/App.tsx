c132723ba421ae32249263f751d5778b931c67af
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-08-07 05:54:04 +0300

    fix(App): добавлены Layout, ContactInformation, Projects и стейты для них

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import { useState } from 'react';
+import { ProjectCardsType } from './components/Projects/Projects.types';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme';
+import Layout from './components/Layout/Layout';
+import ContactInformation from './components/ContactInformation/ContactInformation';
+import Projects from './components/Projects/Projects';
+
 function App() {
-  return <span>Forms & Validation</span>;
+  const [tabsValue, setTabsValue] = useState(0);
+  const [projectCards, setProjectCards] = useState<ProjectCardsType[]>([]);
+  const [projectNumber, setProjectNumber] = useState(1);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Layout tabsValue={tabsValue} setTabsValue={setTabsValue}>
+        {tabsValue === 0 ? (
+          <ContactInformation />
+        ) : (
+          <Projects
+            projectCards={projectCards}
+            setProjectCards={setProjectCards}
+            projectNumber={projectNumber}
+            setProjectNumber={setProjectNumber}
+          />
+        )}
+      </Layout>
+    </ThemeProvider>
+  );
 }
 
 export default App;
