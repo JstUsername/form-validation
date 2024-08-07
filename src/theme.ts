f6106bf1b5c48eb06da0a70189dc3524aa301bfb
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-08-07 05:41:11 +0300

    feat(theme): в проект добавлена тема

---
+++ src/theme.ts
@@ -0,0 +1,46 @@
+import { createTheme } from '@mui/material/styles';
+import '@fontsource/roboto';
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 768,
+      md: 1024,
+      lg: 1440,
+      xl: 1920,
+    },
+  },
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#2196f3',
+      light: 'rgba(33, 150, 243, 0.04)',
+    },
+    background: {
+      default: '#FFFFFF',
+    },
+  },
+  typography: {
+    h6: {
+      fontWeight: 400,
+      fontSize: 16,
+      lineHeight: '175%',
+      letterSpacing: '0.15px',
+    },
+    button: {
+      fontWeight: 400,
+      fontSize: 14,
+      lineHeight: '24px',
+      letterSpacing: '0.4px',
+    },
+    body1: {
+      fontWeight: 400,
+      fontSize: 16,
+      lineHeight: '24px',
+      letterSpacing: '0.4px',
+    },
+  },
+});
+
+export default theme;
