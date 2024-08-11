06b68f61e01b8d621b029a4184d981bb6f82944b
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-08-11 23:10:15 +0300

    fix(Form): Form переименован в FormWrapper, стилизация на основе Box

--- src/commons/FormWrapper/FormWrapper.ts
+++ src/commons/FormWrapper/FormWrapper.ts
@@ -1,6 +1,6 @@
-import { styled } from '@mui/material';
+import { styled, Box } from '@mui/material';
 
-export const Form = styled('form')`
+export const FormWrapper = styled(Box)`
   display: flex;
   flex-direction: column;
   gap: 16px;
