287a6e7f6af807d2aaa8aa5640facbfb56554348
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-08-11 23:12:10 +0300

    fix(ErrorMessage): добавлены по - умолчанию стили typography.body1

--- src/commons/ErrorMessage/ErrorMessage.ts
+++ src/commons/ErrorMessage/ErrorMessage.ts
@@ -1,6 +1,7 @@
 import { styled, Typography } from '@mui/material';
 
 export const ErrorMessage = styled(Typography)`
+  ${({ theme }) => theme.typography.body1};
   margin-left: 2px;
   color: ${({ theme }) => theme.palette.error.main};
 `;
