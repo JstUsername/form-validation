374b294d5f097ae47872f780fe2ffe9ae167c785
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-08-11 23:11:39 +0300

    fix(projectsFormValidation): добавлен .required для skills, исправлено сообщение для end

--- src/schemas/projectsFormValidation.ts
+++ src/schemas/projectsFormValidation.ts
@@ -4,13 +4,17 @@ export const projectsFormValidationSchema = yup.object().shape({
   number: yup.number().required(),
   disabled: yup.boolean().required(),
   title: yup.string().required('Заполните название проекта.'),
-  skills: yup.array().of(yup.string().required()).min(1, 'Выберите хотя бы один навык.').required(),
+  skills: yup
+    .array()
+    .of(yup.string().required())
+    .min(1, 'Выберите хотя бы один навык.')
+    .required('Выберите хотя бы один навык.'),
   role: yup.string().required('Выберите роль на проекте.'),
   beginning: yup.date().required('Введите дату начала работы.').typeError('Введите дату в правильном формате.'),
   end: yup
     .date()
     .nullable()
-    .min(yup.ref('beginning'), 'Дата окончания не может быть меньше даты начала')
+    .min(yup.ref('beginning'), 'Дата окончания не может быть меньше даты начала.')
     .typeError('Введите дату в правильном формате.'),
 });
 
