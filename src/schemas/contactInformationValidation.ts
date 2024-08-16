61dd2d4c5d36b836d5d8100727284e1aaabdd9ba
Андрей Горчаков <g4ndy_c4ndy@mail.ru>
2024-08-16 12:49:02 +0300

    pr(contactInformationValidation): добавлена проверка на буквы в полях с именем, фамилией и отчеством

--- src/schemas/contactInformationValidation.ts
+++ src/schemas/contactInformationValidation.ts
@@ -4,9 +4,15 @@ import { projectsFormValidationSchema } from './projectsFormValidation';
 export const contactInformationValidation = yup.object().shape({
   disabled: yup.boolean().required(),
   error: yup.boolean().required(),
-  surname: yup.string().required('Введите фамилию.'),
-  name: yup.string().required('Введите имя.'),
-  patronymic: yup.string(),
+  surname: yup
+    .string()
+    .matches(/^[А-Яа-яЁёA-Za-z]+$/, 'Введите только буквы.')
+    .required('Введите фамилию.'),
+  name: yup
+    .string()
+    .matches(/^[А-Яа-яЁёA-Za-z]+$/, 'Введите только буквы.')
+    .required('Введите имя.'),
+  patronymic: yup.string().matches(/^[А-Яа-яЁёA-Za-z]+$/, 'Введите только буквы.'),
   phone: yup
     .string()
     .matches(/^\+7\(\d{3}\) \d{3}-\d{2}-\d{2}$/, 'Введите корректный номер.')
