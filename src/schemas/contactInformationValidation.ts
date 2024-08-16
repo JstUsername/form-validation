4b0809353ae3ecf16c0199b5fc42a1676e2d0519
Андрей Горчаков <g4ndy_c4ndy@mail.ru>
2024-08-16 13:07:31 +0300

    pr(schemas): из yup импортированы только используемые сущности

--- src/schemas/contactInformationValidation.ts
+++ src/schemas/contactInformationValidation.ts
@@ -1,30 +1,29 @@
-import * as yup from 'yup';
+import { object, boolean, string, array, InferType } from 'yup';
+
 import { projectsFormValidationSchema } from './projectsFormValidation';
 
-export const contactInformationValidation = yup.object().shape({
-  disabled: yup.boolean().required(),
-  error: yup.boolean().required(),
-  surname: yup
-    .string()
+export const contactInformationValidation = object().shape({
+  disabled: boolean().required(),
+  error: boolean().required(),
+  surname: string()
     .required('Введите фамилию.')
     .matches(/^[А-Яа-яЁёA-Za-z]+$/, 'Введите только буквы.'),
-  name: yup
-    .string()
+  name: string()
     .required('Введите имя.')
     .matches(/^[А-Яа-яЁёA-Za-z]+$/, 'Введите только буквы.'),
-  patronymic: yup
-    .string()
-    .test('is-valid', 'Введите только буквы.', (value) => !value || /^[А-Яа-яЁёA-Za-z]+$/.test(value)),
-  phone: yup
-    .string()
+  patronymic: string().test(
+    'is-valid',
+    'Введите только буквы.',
+    (value) => !value || /^[А-Яа-яЁёA-Za-z]+$/.test(value),
+  ),
+  phone: string()
     .required('Введите номер телефона.')
     .matches(/^\+7\(\d{3}\) \d{3}-\d{2}-\d{2}$/, 'Введите корректный номер.'),
-  email: yup.string().email('Введите корректный email.'),
-  activity: yup
-    .boolean()
+  email: string().email('Введите корректный email.'),
+  activity: boolean()
     .test('is-checked', 'Отметьте чекбокс.', (value) => value === true)
     .required(),
-  projectsArray: yup.array(projectsFormValidationSchema).required().min(1, 'Добавьте минимум один проект.'),
+  projectsArray: array(projectsFormValidationSchema).required().min(1, 'Добавьте минимум один проект.'),
 });
 
-export type ContactInformationValidationType = yup.InferType<typeof contactInformationValidation>;
+export type ContactInformationValidationType = InferType<typeof contactInformationValidation>;
--- src/schemas/projectsFormValidation.ts
+++ src/schemas/projectsFormValidation.ts
@@ -1,21 +1,19 @@
-import * as yup from 'yup';
+import { object, string, array, boolean, ref, date, number, InferType } from 'yup';
 
-export const projectsFormValidationSchema = yup.object().shape({
-  number: yup.number().required(),
-  disabled: yup.boolean().required(),
-  title: yup.string().required('Заполните название проекта.'),
-  skills: yup
-    .array()
-    .of(yup.string().required())
+export const projectsFormValidationSchema = object().shape({
+  number: number().required(),
+  disabled: boolean().required(),
+  title: string().required('Заполните название проекта.'),
+  skills: array()
+    .of(string().required())
     .min(1, 'Выберите хотя бы один навык.')
     .required('Выберите хотя бы один навык.'),
-  role: yup.string().required('Выберите роль на проекте.'),
-  beginning: yup.date().required('Введите дату начала работы.').typeError('Введите дату в правильном формате.'),
-  end: yup
-    .date()
+  role: string().required('Выберите роль на проекте.'),
+  beginning: date().required('Введите дату начала работы.').typeError('Введите дату в правильном формате.'),
+  end: date()
     .nullable()
-    .min(yup.ref('beginning'), 'Дата окончания не может быть меньше даты начала.')
+    .min(ref('beginning'), 'Дата окончания не может быть меньше даты начала.')
     .typeError('Введите дату в правильном формате.'),
 });
 
-export type ProjectsFormValidationSchemaType = yup.InferType<typeof projectsFormValidationSchema>;
+export type ProjectsFormValidationSchemaType = InferType<typeof projectsFormValidationSchema>;
